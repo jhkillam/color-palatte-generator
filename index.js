fb34dec5059cb2904a79129794893db5031f9c21
Joey Killam <jhkillam@gmail.com>
2019-11-17 11:12:29 -0600

    random colors working, todo:lock btn

--- index.js
+++ index.js
@@ -9,54 +9,113 @@
 //   return randomColor
 // }
 
-class ColorBox extends React.Component {
+// class ColorBox extends React.Component {
 
+//   state = {
+//     color: "blue"
+//   }
+
+//   generateRandomColor = () => {
+//     let randomColor = ("#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16)})).toUpperCase()
+//     console.log(randomColor)
+//     this.setState({
+//       color: randomColor
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div style={{ backgroundColor: this.state.color }} className="w-100 d-flex flex-column align-items-center justify-content-center">
+//         <h1>{this.state.color}</h1>
+//         <button className="btn btn-dark">UNLOCK</button>
+//       </div>
+//     )
+//   }
+// }
+
+// const RandomButtonEl = () => {
+//   let newColor = ColorBox.generateRandomColor
+//   console.log(newColor)
+//   return (
+//       <div className="text-center bg-dark fixed-top">
+//         <button className="btn btn-secondary m-1">RANDOMIZE COLORS</button>  
+//       </div>
+//   )
+// }
+
+function Box(props, index) {
+  const boxColor = {
+    backgroundColor: props.color,
+  }
+  const boxStyles = {
+    padding: "0 7px", 
+    backgroundColor: "#343A40", 
+    color: "#FFFFFF", 
+    borderRadius: ".25rem"
+  }
+  let lockBtnTxt = 'LOCK'
+  let lockBtnTxtColor = {
+    color: '#FFFFFF'
+  }
+  if (props.isLocked) {
+    btnTxt = 'UNLOCK'
+    lockBtnTxtColor.color = '#eeeeee'
+  }
+  return (
+    <div key={index} style={boxColor} className="w-100 d-flex flex-column align-items-center justify-content-center">
+      <h2 style={boxStyles}>{props.color}</h2>
+      <button style={lockBtnTxtColor} className="btn btn-dark">{lockBtnTxt}</button>
+    </div>
+  )
+}
+
+class App extends React.Component {
   state = {
-    color: "blue"
+    boxes: [
+      this._Box(),
+      this._Box(),
+      this._Box(),
+      this._Box(),
+      this._Box()
+    ]
   }
 
-  generateRandomColor = () => {
-    let randomColor = ("#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16)})).toUpperCase()
-    console.log(randomColor)
-    this.setState({
-      color: randomColor
+  _randomColor() {
+    return ("#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16)})).toUpperCase()
+  }
+
+  _Box() {
+    return {
+      color: this._randomColor(),
+      isLocked: false
+    }
+  }
+
+  randomize() {
+    const currentBoxes = [...this.state.boxes]
+    const newBoxes = currentBoxes.map((box) => {
+      if (!box.isLocked) {
+        box.color = this._randomColor();
+      }
+      return box;
     })
+    this.setState({boxes: newBoxes})
   }
 
   render() {
+
     return (
-      <div style={{ backgroundColor: this.state.color }} className="w-100 d-flex flex-column align-items-center justify-content-center">
-        <h1>{this.state.color}</h1>
-        <button className="btn btn-dark">UNLOCK</button>
+      <div>
+        <div className="text-center bg-dark fixed-top">
+          <button onClick={() => this.randomize()} className="btn btn-secondary m-1">RANDOMIZE COLORS</button>  
+        </div>
+        <div className="w-100 d-flex" style={{ height: "100vh" }}>
+          {this.state.boxes.map(Box)}
+        </div>
       </div>
     )
   }
-}
-
-const RandomButtonEl = () => {
-  let newColor = ColorBox.generateRandomColor
-  console.log(newColor)
-  return (
-      <div className="text-center bg-dark fixed-top">
-        <button className="btn btn-secondary m-1">RANDOMIZE COLORS</button>  
-      </div>
-  )
-}
-
-const App = function() {
-
-  return (
-    <div>
-      <RandomButtonEl/>
-      <div className="w-100 d-flex" style={{ height: "100vh" }}>
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-        <ColorBox />
-      </div>
-    </div>
-  )
+  
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'))
